3c2708b022debb19e6d7d214107008a7b40d6576
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-06-01 08:36:35 -0400

    chore(TS): add typescript definitions (#1)
    * chore(TS): add typescript definitions
    
    * chore(TS): fix resolution issues
    
    * chore(TS): make end parameter optional
    
    * chore(TS): fix definitions

---
+++ type-definitions/index.d.ts
@@ -0,0 +1,24 @@
+import { Stream } from "most";
+
+declare interface Thenable<A> {
+    then<U>(onFulfilled?: (value: A) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Thenable<U>;
+    then<U>(onFulfilled?: (value: A) => U | Thenable<U>, onRejected?: (error: any) => void): Thenable<U>;
+    catch<U>(onRejected?: (error: any) => U | Thenable<U>): Thenable<U>;
+}
+
+declare interface Promise<A> {
+  constructor(callback: (resolve: (value?: A | Thenable<A>) => void, reject: (error?: any) => void) => void): Promise<A>;
+
+  then<U>(onFulfilled?: (value: A) => U | Thenable<U>, onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
+  then<U>(onFulfilled?: (value: A) => U | Thenable<U>, onRejected?: (error: any) => void): Promise<U>;
+
+  catch<U>(onRejected?: (error: any) => U | Thenable<U>): Promise<U>;
+}
+
+declare interface DisposeFn {
+  (): void | Promise<any>;
+}
+
+export default function create<A>(f: (add: (a: A) => void,
+                                      end: (x?: A) => void,
+                                      error: (e: Error) => void) => void | DisposeFn ): Stream<A>;
