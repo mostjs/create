ce63a4e9493814c4efd184de0672f9c20099d256
Brian Cavalier <brian@hovercraftstudios.com>
2016-06-02 07:33:39 -0400

    Fix error time value

--- src/PropagateAllTask.js
+++ src/PropagateAllTask.js
@@ -1,8 +1,9 @@
 /** @license MIT License (c) copyright 2010-2016 original author or authors */
 
 export default class PropagateAllTask {
-  constructor (sink, events) {
+  constructor (sink, time, events) {
     this.sink = sink
+    this.time = time
     this.events = events
   }
 
@@ -13,6 +14,7 @@ export default class PropagateAllTask {
 
     for (let i = 0, l = events.length; i < l; ++i) {
       event = events[i]
+      this.time = event.time
       sink.event(event.time, event.value)
     }
 
@@ -20,6 +22,6 @@ export default class PropagateAllTask {
   }
 
   error (e) {
-    this.sink.error(0, e)
+    this.sink.error(this.time, e)
   }
 }
